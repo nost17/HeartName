a13478642140e90a22beee1b06d017622280425f
nost <erick.personal16@outlook.com>
2026-05-28 18:28:46 -0300

    Se arreglo la colorizacion

--- go.mod
+++ go.mod
@@ -2,10 +2,23 @@ module polare/HtSize
 
 go 1.26.3
 
-require github.com/fatih/color v1.19.0
+require golang.org/x/term v0.43.0
 
 require (
-	github.com/mattn/go-colorable v0.1.14 // indirect
-	github.com/mattn/go-isatty v0.0.20 // indirect
-	golang.org/x/sys v0.42.0 // indirect
+	charm.land/lipgloss/v2 v2.0.3 // indirect
+	github.com/charmbracelet/colorprofile v0.4.3 // indirect
+	github.com/charmbracelet/ultraviolet v0.0.0-20251205161215-1948445e3318 // indirect
+	github.com/charmbracelet/x/ansi v0.11.7 // indirect
+	github.com/charmbracelet/x/term v0.2.2 // indirect
+	github.com/charmbracelet/x/termios v0.1.1 // indirect
+	github.com/charmbracelet/x/windows v0.2.2 // indirect
+	github.com/clipperhouse/displaywidth v0.11.0 // indirect
+	github.com/clipperhouse/uax29/v2 v2.7.0 // indirect
+	github.com/lucasb-eyer/go-colorful v1.4.0 // indirect
+	github.com/mattn/go-runewidth v0.0.23 // indirect
+	github.com/muesli/cancelreader v0.2.2 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
+	golang.org/x/sync v0.18.0 // indirect
+	golang.org/x/sys v0.44.0 // indirect
 )
--- main.go
+++ main.go
@@ -8,16 +8,46 @@ import (
 	"strings"
 	"time"
 
-	"github.com/fatih/color"
+	"charm.land/lipgloss/v2"
+	"golang.org/x/term"
 )
 
 var reader *bufio.Scanner = bufio.NewScanner(os.Stdin)
 
+var estilo = lipgloss.NewStyle().Foreground(lipgloss.Green)
+var estiloNombre = lipgloss.NewStyle().Foreground(lipgloss.White).Bold(true)
+var AnchoDeTerminal int = obtenerAnchoTerminal()
+var EspacioParaCentrar string
+
 const Caracter string = "#"
-const AcentoSeleccionado color.Attribute = color.FgGreen
+const CentrarDibujo = false
+const EspacioParaDibujo = 4
+
+// const AcentoSeleccionado color.Attribute = color.FgGreen
 
 func esperar() {
-	time.Sleep((1 * time.Second) / 3)
+	time.Sleep((1 * time.Second) / 25)
+}
+
+func obtenerAnchoTerminal() int {
+	anchoTerminal, _, err := term.GetSize(int(os.Stdout.Fd()))
+	if err != nil {
+		fmt.Println("Error al obtener el tamaño de la terminal:", err)
+		panic(1)
+	}
+	return anchoTerminal
+}
+
+func generarEspacio(tamañoForma int) string {
+	var tamañoEspacio int
+
+	if CentrarDibujo {
+		tamañoEspacio = (AnchoDeTerminal - tamañoForma) / 2
+	} else {
+		tamañoEspacio = EspacioParaDibujo
+	}
+
+	return strings.Repeat(" ", tamañoEspacio)
 }
 
 func leerTexto(msg string) string {
@@ -51,7 +81,7 @@ func leerNumero(msg string, error_msg string) int {
 	}
 }
 
-func imprimirNombre(nombre string, linea string, salida *color.Color) {
+func dibujarNombre(nombre string, linea string, salida *strings.Builder) {
 	n := len(nombre)
 	anchoLinea := len(linea)
 	anchoNombre := n
@@ -67,32 +97,56 @@ func imprimirNombre(nombre string, linea string, salida *color.Color) {
 	anchoNombreIzq := len(nombre[:anchoNombre]) + 1
 	anchoNombreDer := len(nombre[anchoNombre:]) + 1
 	nombre = fmt.Sprintf(" %s ", nombre)
-	nombreColoreado := color.New(color.FgWhite, color.Bold).Sprint(nombre)
-	nombreIndice := strings.Index(nombreColoreado, nombre)
-	salida.Print(linea[:anchoLinea-anchoNombreIzq])
-	for i := range nombreColoreado {
-		fmt.Print(string(nombreColoreado[i]))
-		if i >= nombreIndice && i <= nombreIndice+n {
-			esperar()
-		}
-	}
-	salida.Println(linea[:anchoLinea-anchoNombreDer])
+	salida.WriteString(EspacioParaCentrar)
+	salida.WriteString(linea[:anchoLinea-anchoNombreIzq])
+	salida.WriteString(estiloNombre.Render(nombre))
+	salida.WriteString(estilo.Render(linea[:anchoLinea-anchoNombreDer]))
+	salida.WriteString("\n")
 }
-func imprimirPicos(linea string, tamañoPico, tamañoMaximo int) {
+func dibujarPicos(linea string, tamañoPico int, salida *strings.Builder) {
+	tamañoMaximo := len(linea) / 2
 	for i := tamañoPico; i <= tamañoMaximo; i += 2 {
 		relleno := linea[:(i + 1)]
 		espacio := strings.Repeat(" ", (tamañoMaximo-i)/2)
-		fmt.Print(espacio, relleno, espacio, espacio, relleno, "\n")
+		salida.WriteString(EspacioParaCentrar)
+		salida.WriteString(espacio)
+		salida.WriteString(relleno)
+		salida.WriteString(espacio)
+		salida.WriteString(espacio)
+		salida.WriteString(relleno)
+		salida.WriteString("\n")
+		// fmt.Print(espacio, relleno, espacio, espacio, relleno, "\n")
 		// fmt.Print(len(relleno), len(espacio), i, maximo, tamañoExtra, "\n")
 	}
 }
 
-func imprimirCuerpo(linea string, tamañoPico, tamañoMaximo int) {
+func dibujarCuerpo(linea string, tamañoPico int, salida *strings.Builder) {
+	tamañoMaximo := len(linea) / 2
 	for i := (tamañoMaximo * 2) - 2; i > tamañoPico+(tamañoPico/2); i -= 2 {
 		relleno := linea[:i]
 		espacio := strings.Repeat(" ", ((tamañoMaximo*2)-i)/2)
-		fmt.Print(espacio, relleno, "\n")
+		salida.WriteString(EspacioParaCentrar)
+		salida.WriteString(espacio)
+		salida.WriteString(relleno)
+		salida.WriteString("\n")
+		// fmt.Print(len(relleno), "-")
 	}
+}
+
+func imprimirTexto(texto string) {
+	var saltarEspera bool = false
+	for i := range texto {
+		letra := string(texto[i])
+		if letra == "[" {
+			saltarEspera = true
+		} else if letra == "m" && saltarEspera {
+			saltarEspera = false
+		} else if !saltarEspera && letra != " " {
+			esperar()
+		}
+		lipgloss.Print(letra)
+	}
+	// lipgloss.Println(texto)
 }
 
 func main() {
@@ -108,11 +162,12 @@ func main() {
 	*/
 	maximo := (tamaño * 2) + tamañoPico - 1
 	linea := strings.Repeat(Caracter, maximo*2)
-
-	salidaColor := color.New(AcentoSeleccionado)
-
-	imprimirPicos(linea, tamañoPico, maximo)
-	imprimirNombre(nombre, linea, salidaColor)
-	imprimirCuerpo(linea, tamañoPico, maximo)
+	EspacioParaCentrar = generarEspacio(maximo * 2)
+	salida := strings.Builder{}
+	salida.WriteString("\n")
+	dibujarPicos(linea, tamañoPico, &salida)
+	dibujarNombre(nombre, linea, &salida)
+	dibujarCuerpo(linea, tamañoPico, &salida)
+	imprimirTexto(estilo.Render(salida.String()))
 
 }
